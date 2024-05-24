9f61eccf87c4970e8a9af24eaacf3b7956fac548
Soroush Alinia <soroushalinia.wm@gmail.com>
2024-05-24 19:26:26 +0330

    [feat] update api for teacher page

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -9,24 +9,40 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
-  const baseUrl = process.env.API_URL;
-  const res = await fetch(`${baseUrl}/api/teacher`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch teachers");
+  try {
+    const baseUrl = process.env.API_URL;
+    const res = await fetch(`${baseUrl}/api/teacher`, {
+      next: { revalidate: 10 },
+    });
+    if (!res.ok) {
+      return null;
+    }
+    return res.json();
+  } catch (e) {
+    return null;
   }
-  return res.json();
 }
 
 export default async function About() {
   const teachers = await getData();
+  if (teachers === null) {
+    return (
+      <div className="h-screen text-center flex flex-col gap-12 items-center justify-center">
+        <h1 className="text-8xl font-bold text-primary">خطا</h1>
+        <h2 className="text-primary font-bold text-5xl">امکان اتصال به سرور وجود ندارد</h2>
+        <Link className="text-primary font-bold text-2xl" href="/">بازگشت به صفحه اصلی</Link>
+      </div>
+    );
+  }
   const teachersView = teachers.map((teacher: any) => {
     return (
       <Card key={teacher.name}>
         <CardHeader>
           <CardTitle>
-            <div className="flex flex-col items-center justify-center gap-3">
+            <div className="pt-8 flex flex-col items-center justify-center gap-3">
               {teacher.image === null ? (
                 <IoPersonCircleOutline className="w-[200px] h-[200px] text-gray-500" />
               ) : (
@@ -44,7 +60,10 @@ export default async function About() {
           <CardDescription>{teacher.title}</CardDescription>
         </CardHeader>
         <CardContent>
-          <p dangerouslySetInnerHTML={{ __html: teacher.describe }}></p>
+          <p
+            className=""
+            dangerouslySetInnerHTML={{ __html: teacher.describe }}
+          ></p>
         </CardContent>
         <CardFooter className="flex flex-row-reverse justify-center gap-2 lg:gap-6">
           <a
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
           {
             protocol: 'http',
             hostname: '127.0.0.1',
-            port: '8500',
+            port: '8000',
             pathname: '/**',
           },
           {
